801f43859393bd6eb0773afc02a9828c1fd66f47
JubayerAlFaruk <jubayeralfaruk@gmail.com.com>
2025-08-24 13:10:05 +0600

    Withdraw btn Dynamic

--- home.js
+++ home.js
@@ -1,22 +1,23 @@
 // bank-account | amount | user-pin-number | add-money-btn
 
-
+    const userPin = 1234;
+    
+// // add money feature
 document.getElementById("add-money-btn").addEventListener("click", function (e) {
     e.preventDefault();
-    const userPin = 1234;
     const bankName = document.getElementById("bank-name").value;
     
     const bankAccount = document.getElementById("bank-account").value;
 
     const inputAmount = parseInt(document.getElementById("amount").value);
 
-    const inputPin = parseInt(document.getElementById("user-pin-number").value);
+    const addMoneyPin = parseInt(document.getElementById("add-money-pin-number").value);
 
     if (bankName === "Select bank") {
         alert("Select A Bank.")
         return;
     }
-    if (bankAccount.length !== 11 || userPin !== inputPin) {
+    if (bankAccount.length !== 11 || userPin !== addMoneyPin) {
         alert("Please, User Valid Account & Password")
         return;
     }
@@ -25,7 +26,6 @@ document.getElementById("add-money-btn").addEventListener("click", function (e) 
     }
 
     const currentAmount = parseInt(document.getElementById("current-amount").innerText);
-
     const newAccountAmount = currentAmount + inputAmount;
 
     document.getElementById("current-amount").innerText = newAccountAmount
@@ -35,7 +35,42 @@ document.getElementById("add-money-btn").addEventListener("click", function (e) 
     
 })
 
+// // cash out feature
 
+document.getElementById("withdraw-btn").addEventListener("click", function (e) {
+    e.preventDefault();
+    const agentNumber = document.getElementById("agent-number").value
+    const withdrawAmount = parseInt(document.getElementById("withdraw-amount").value)
+    const withdrawPinNumber = parseInt(document.getElementById("withdraw-pin-number").value)
+    
+    if (agentNumber.length !== 11) {
+        alert("Enter 11 digit valid agent number.")
+        return;
+    }
+
+    if (withdrawAmount < 5) {
+        alert("Enter Valid Amount.")
+        return;
+    }
+
+    if (withdrawPinNumber !== userPin) {
+        alert("Invalid Pin number");
+    }
+
+    const currentAmount = parseInt(document.getElementById("current-amount").innerText);
+    const newBalance = currentAmount - withdrawAmount
+    if (currentAmount>=newBalance) {
+        document.getElementById("current-amount").innerText = newBalance;
+    }
+    alert("Insufficient Balance");
+})
+
+
+
+
+
+
+// // Toggling feature
 document.getElementById('add-money').addEventListener("click", function () {
     document.getElementById('cashOutForm').style.display = "none";
     document.getElementById('addMoneyForm').style.display = "block";
@@ -45,3 +80,6 @@ document.getElementById('cashout').addEventListener("click", function () {
     document.getElementById('addMoneyForm').style.display = "none";
     document.getElementById('cashOutForm').style.display = "block";
 })
+
+
+
